abc5adcda01ba5e8024584f2372829f7fb24999d
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-07-09 00:26:06 +0200

    display companies in table instead of list

--- modules/companies/client/controllers/list-companies.client.controller.js
+++ modules/companies/client/controllers/list-companies.client.controller.js
@@ -5,11 +5,18 @@
     .module('companies')
     .controller('CompaniesListController', CompaniesListController);
 
-  CompaniesListController.$inject = ['CompaniesService'];
+  CompaniesListController.$inject = ['CompaniesService', 'dateFilter'];
 
-  function CompaniesListController(CompaniesService) {
+  function CompaniesListController(CompaniesService, dateFilter) {
     var vm = this;
 
+    vm.dateFilter = dateFilter;
+
     vm.companies = CompaniesService.query();
+
+    vm.removeCompany = function (index, companies) {
+      var a = 1;
+    };
+
   }
 })();
