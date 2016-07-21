04af1fa9a3dddc436b8756e6eccaebd2b142af31
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-07-21 23:40:30 +0200

    update fair-info

--- modules/companies/server/models/company.server.model.js
+++ modules/companies/server/models/company.server.model.js
@@ -22,16 +22,18 @@ var CompanySchema = new Schema({
     trim: true
   },
   fair_info: {
-    description: {
-      type: String,
-      default: '',
-      trim: true
-    },
-    funfacts: {
-      type: String,
-      default: '',
-      trim: true
-    },
+    description: { type: String, default: '', trim: true },
+    funfacts: { type: String, default: '', trim: true },
+    facebook: { type: String, default: '', trim: true },
+    twitter: { type: String, default: '', trim: true },
+    youtube: { type: String, default: '', trim: true },
+    linkedin: { type: String, default: '', trim: true },
+    email: { type: String, default: '', trim: true },
+    web: { type: String, default: '', trim: true },
+    map: { type: String, default: '', trim: true },
+    banner: { type: String, default: '', trim: true },
+    employees_global: { type: Number, default: 0 },
+    employees_sweden: { type: Number, default: 0 },
     offers: [{ type: String, enum: ['Sommarjobb', 'Exjobb'] }],
     sections: [{ type: String, enum: ['F', 'A', 'D', 'E', 'M', 'K', 'I'] }],
   },
